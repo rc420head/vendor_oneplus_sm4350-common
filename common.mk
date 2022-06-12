LineageOS/android_device_oneplus_sm8350-common

sm8350-common: init: Use the same PID/VIDs from OOS
Change-Id: Ieaa52f748c75272b80fa6f3e7018cdb71e3a2f7f

common.mk
@@ -319,6 +319,7 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.oplus.display.rc \
     init.oplus.sensors.rc \
+    init.oplus.usb.rc \
     init.qcom.early_boot.sh \
     init.qcom.rc \
     init.qcom.recovery.rc \

rc420head/vendor_oneplus_sm4350-common

common.mk
@@ -214,6 +214,7 @@ PRODUCT_PACKAGES += \
     init.oem.rc \
     init.oem_ftm.rc \
     init.oem_rf.rc \
+    init.oem.usb.rc \
     init.qcom.class_core.sh \
     init.qcom.early_boot.sh \
     init.qcom.factory.rc \